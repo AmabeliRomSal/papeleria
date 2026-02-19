e515b16d2b5a3684b86232de999abb4c38e280d8
AmabRomS <amabeli.romero.s@gmail.xom>
2026-02-18 18:37:04 -0600

    product-card

---
+++ src/app/services/producto.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { Product } from "../models/producto.model";
+
+@Injectable ({provided: 'root'}.)
+export class ProductsService{
+    private readonly products Product[]= [
+        
+    ];
+}
